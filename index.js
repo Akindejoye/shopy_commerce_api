cf1c1c83acf101c1024675cf75e0134c52c853d4
Akindejoye <tydruhill@gmail.com>
2022-01-25 10:53:51 +0100

    Implementing Routes And Stripe Payment

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 const userRoute = require("./Routes/user");
 const authRoute = require("./Routes/auth");
+const productRoute = require("./Routes/product");
+const cartRoute = require("./Routes/cart");
+const orderRoute = require("./Routes/order");
+const stripeRoute = require("./Routes/stripe");
 
 dotenv.config();
 
@@ -19,6 +23,10 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/carts', cartRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("The Backend Server is running");
